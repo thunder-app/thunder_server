842260893044cd97c5483eac25aa123fd70f11e3
Micah Morrison <micahmorrison95@gmail.com>
2024-05-10 15:01:36 -0400

    Create Thunder database if not exists

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Configure database
 const sequelize = new Sequelize(
-  `postgres://${process.env.POSTGRES_USER ?? "user"}:${process.env.POSTGRES_PASSWORD ?? "password"}@${process.env.POSTGRES_HOSTNAME ?? "postgres"}:${process.env.POSTGRES_PORT ?? "5432"}/${process.env.POSTGRES_DATABASE ?? "thunder-database"}`
+  `postgres://${process.env.POSTGRES_USER || "postgres"}:${process.env.POSTGRES_PASSWORD || "password"}@${process.env.POSTGRES_HOSTNAME || "postgres"}:${process.env.POSTGRES_PORT || "5432"}/${process.env.POSTGRES_DATABASE || "thunder_database"}`
 );
 
 export default sequelize;
